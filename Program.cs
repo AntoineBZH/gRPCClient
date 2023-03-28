a93f676b89642738cd48c4d6cb4dd398c5637980
Antoine HOUSSAIS <ahoussais@apsdigit.lan>
2023-03-28 09:14:06 +0200

    Déclaration factuelle de la tâche qui était induite précédemment.

--- Program.cs
+++ Program.cs
@@ -2,14 +2,18 @@
 using Grpc.Net.Client;
 using gRPCClient;
 
-// The port number must match the port of the gRPC server.
-using var channel = GrpcChannel.ForAddress("https://localhost:5001/");
-var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(
-                new HelloRequest { Name = "Eric" });
-Console.WriteLine("Greeting: " + reply.Message);
-reply = await client.SayHelloAsync(
-                new HelloRequest { Name = "Antoine" });
-Console.WriteLine("Greeting: " + reply.Message);
-Console.WriteLine("Press any key to exit...");
-Console.ReadKey();
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // The port number must match the port of the gRPC server.
+        using var channel = GrpcChannel.ForAddress("https://localhost:5001/");
+        var client = new Greeter.GreeterClient(channel);
+        var reply = await client.SayHelloAsync(
+                        new HelloRequest { Name = "Eric" });
+        Console.WriteLine("Greeting: " + reply.Message);
+        reply = await client.SayHelloAsync(
+                        new HelloRequest { Name = "Antoine" });
+        Console.WriteLine("Greeting: " + reply.Message);
+    }
+}
